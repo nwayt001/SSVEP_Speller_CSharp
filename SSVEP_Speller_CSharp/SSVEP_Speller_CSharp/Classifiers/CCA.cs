27f3bbc30dbdbd56a1259e4fa0bb21835d208b8c
nwayt001 <nick.waytowich@gmail.com>
2016-11-21 16:26:11 -0500

    Started CCA classifier

---
+++ SSVEP_Speller_CSharp/SSVEP_Speller_CSharp/Classifiers/CCA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace SSVEP_Speller_CSharp.Classifiers
+{
+    #region Fields
+    
+    #endregion Fields
+
+    class CCA
+    {
+        
+    }
+}
